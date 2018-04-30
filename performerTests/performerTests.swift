2ef531fead6f5aa417a4f64546d7d22067c1efc7
nonomura-taku <nonomura.taku@dena.jp>
2018-04-30 18:31:50 +0900

    Xcodeプロジェクト追加

---
+++ performerTests/performerTests.swift
@@ -0,0 +1,36 @@
+//
+//  performerTests.swift
+//  performerTests
+//
+//  Created by Taku Nonomura on 2018/04/30.
+//  Copyright © 2018年 visioooon. All rights reserved.
+//
+
+import XCTest
+@testable import performer
+
+class performerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

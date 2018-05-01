f6140091274f5bc106f0af01b6b13f6dca7f5de9
nonomura-taku <nonomura.taku@dena.jp>
2018-05-01 15:02:07 +0900

    AudioPlayerViewの上にSubViewを表示されるようにした

--- performer/Scripts/ViewController.swift
+++ performer/Scripts/ViewController.swift
@@ -12,45 +12,58 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    // AVPlayer.
+    var videoPlayer : AVPlayer!
+    
     override func viewDidLoad() {
+
         // 親クラスのLoad完了処理.
         super.viewDidLoad()
         
-        // ボタンを生成
-        let button = UIButton(type: .system)
-        button.setTitle("Play", for: .normal)
-        button.addTarget(self, action: #selector(self.playMovieFromProjectBundle), for: .touchUpInside)
-        button.sizeToFit()
-        button.center = self.view.center
-        self.view.addSubview(button)
-    }
-
-    // アプリにBundleされたMovieFileを再生します.
-    @objc func playMovieFromProjectBundle() {
+        // VideoPlayerの生成.
         if let bundlePath = Bundle.main.path(forResource: "Assets/baseball1", ofType: "mov") {
             
-            let videoPlayer = AVPlayer(url: URL(fileURLWithPath: bundlePath))
+            // Player.
+            videoPlayer = AVPlayer(url: URL(fileURLWithPath: bundlePath))
             
-            // 動画プレイヤーの用意
-            let playerController = AVPlayerViewController()
-            playerController.showsPlaybackControls = false;
-            playerController.player = videoPlayer
+            // Viewを生成.
+            let videoPlayerView = AVPlayerView(frame: self.view.bounds)
             
-            // AVPlayerViewControllerへの遷移処理.
-            // 遷移完了で、再生.
-            self.present(playerController, animated: true, completion: {
-                videoPlayer.play()
-            })
+            // UIViewのレイヤーをAVPlayerLayerにする.
+            let playerLayer = videoPlayerView.layer as! AVPlayerLayer
+            playerLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+            playerLayer.player = videoPlayer
+            self.view.layer.addSublayer(playerLayer)
+            
+            // 動画の再生ボタンを生成.
+            let startButton = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            startButton.layer.position = CGPoint(x: self.view.bounds.midX, y: self.view.bounds.maxY - 50)
+            startButton.layer.masksToBounds = true
+            startButton.layer.cornerRadius = 20.0
+            startButton.backgroundColor = UIColor.orange
+            startButton.setTitle("Start", for: UIControlState.normal)
+            startButton.addTarget(self, action: #selector(self.onStartButtonClick), for: UIControlEvents.touchUpInside)
+            self.view.addSubview(startButton)
+            
         } else {
             print("no such file")
-        }
+            return
+        }        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @objc func onStartButtonClick(sender: UIButton) {
+        if sender.titleLabel?.text == "Start" {
+            videoPlayer.play()
+            sender.setTitle("Stop", for: UIControlState.normal)
+        } else {
+            videoPlayer.pause()
+            sender.setTitle("Start", for: UIControlState.normal)
+        }
+    }
 }
 

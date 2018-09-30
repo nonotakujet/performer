2ad155593b038b77fd43346441294d3edca6e335
nonomura.taku <nonomura.taku@dena.jp>
2018-10-01 01:36:27 +0900

    table viewを表示

---
+++ performer/Scripts/View/TopViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TopViewCell.swift
+//  performer
+//
+//  Created by Taku Nonomura on 2018/10/01.
+//  Copyright © 2018年 visioooon. All rights reserved.
+//
+
+import UIKit
+
+class TopViewCell: UITableViewCell {
+    @IBOutlet weak var thumbnailImage: UIImageView!
+}

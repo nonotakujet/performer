3320aa90f9e1a4bc619ffbd20095cf64100113ad
nonomura.taku <nonomura.taku@dena.jp>
2019-01-02 06:38:46 +0900

    動画撮影ViewControllerを用意

---
+++ performer/Scripts/ViewController/MovieCaptureViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieCaptureViewController.swift
+//  performer
+//
+//  Created by Taku Nonomura on 2019/01/02.
+//  Copyright © 2019年 visioooon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieCaptureViewController: UIViewController {
+    override func viewDidLoad() {
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
